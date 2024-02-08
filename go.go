1a7e19cdfa38a8d31b63572fcede19f395ffede8
dwd <wenyi173@gmail.com>
2024-02-09 00:07:11 +0800

    add type: Method, string type

--- go.go
+++ go.go
@@ -2,6 +2,15 @@ package rest
 
 import "net/http"
 
+type Method string
+
+const (
+	GET    Method = "GET"
+	POST   Method = "POST"
+	PUT    Method = "PUT"
+	DELETE Method = "DELETE"
+)
+
 type Header map[string]string
 
 type Response struct {
